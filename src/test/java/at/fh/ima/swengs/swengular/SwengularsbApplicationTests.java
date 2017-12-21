b2b56bd33e0cf9f99efb4f08a40e287976058031
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-12-21 22:18:10 +0100

    Implemented first testcases for save Genre and Movie

--- src/test/java/at/fh/ima/swengs/swengular/SwengularsbApplicationTests.java
+++ src/test/java/at/fh/ima/swengs/swengular/SwengularsbApplicationTests.java
@@ -1,16 +1,56 @@
 package at.fh.ima.swengs.swengular;
 
+import at.fh.ima.swengs.swengular.model.Genre;
+import at.fh.ima.swengs.swengular.repository.GenreRepository;
+import at.fh.ima.swengs.swengular.repository.MovieRepository;
+import at.fh.ima.swengs.swengular.service.TmdbAPI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SwengularsbApplicationTests {
+public class SwengularsbApplicationTests
+{
+    @Autowired
+    GenreRepository genreRepository;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Autowired
+    TmdbAPI tmdbAPI;// = new TmdbAPI();
+
+
+    //PASS
+	//@Test
+	public void saveGenre()
+	{
+        Genre g1 = new Genre(1, "Action");
+        genreRepository.save(g1);
+    }
+
+    //PASS
+    //@Test
+    public void saveAllGenres()
+    {
+        //Repository is able to save collection
+        genreRepository.save(tmdbAPI.mapAllTmdbGenres());
+    }
+
+    //FAIL
+    @Test
+    public void saveMovie()
+    {
+        //TmdbAPI tmdbAPI = new TmdbAPI();
+
+        movieRepository.save(tmdbAPI.mapMovie(tmdbAPI.getTmdbMovies().getMovie(493960, "en")));
+    }
+
+
+
+
 
 }
