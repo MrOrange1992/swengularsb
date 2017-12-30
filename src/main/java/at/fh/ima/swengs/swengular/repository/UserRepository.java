d79a09b8f72e55fc8a5f028ebfe0e10efa875bb5
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-12-30 15:09:26 +0100

    Work on user rep functions

--- src/main/java/at/fh/ima/swengs/swengular/repository/UserRepository.java
+++ src/main/java/at/fh/ima/swengs/swengular/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package at.fh.ima.swengs.swengular.repository;
 
-import at.fh.ima.swengs.swengular.model.Genre;
+//import at.fh.ima.swengs.swengular.model.Genre;
 import at.fh.ima.swengs.swengular.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,11 +11,25 @@ import java.util.Set;
 @RepositoryRestResource
 public interface UserRepository extends PagingAndSortingRepository<User, Long>
 {
-    //find user by last/first name
-    Set<User> findByLastName(@Param("lastName") String lastName);
-    Set<User> findByFirstName(@Param("firstName") String firstName);
-    Set<User> findByLastNameAndFirstName(@Param("lastName") String lastName, @Param("firstName") String firstName);
+    //Get all users
+    Set<User> findBy();
 
-    Set<User> findByGenres(@Param("genre") Genre genre);    //TODO: don't know if this works
+    User findById(@Param("id") Long id);
+
+    User findByFirstName(@Param("firstName") String firstName);
+
+    User findByLastName(@Param("lastName") String lastName);
+
+    User findByLastNameAndFirstName(@Param("lastName") String lastName, @Param("firstName") String firstName);
+
+
+    Set<User> findAllByLastName(@Param("lastName") String lastName);
+
+    Set<User> findAllByFirstName(@Param("firstName") String firstName);
+
+    Set<User> findAllByLastNameAndFirstName(@Param("lastName") String lastName, @Param("firstName") String firstName);
+
+
+    Set<User> findAllByGenreIDsContaining(@Param("genreID") int genreID);
 
 }
