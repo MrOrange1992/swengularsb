56c103cd80db82e63575efa10f68eb7025cc9d13
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-12-30 15:10:20 +0100

    Repository deprecated, will use MovieDb class entirely from TMDB API

--- src/main/java/at/fh/ima/swengs/swengular/repository/MovieRepository.java
+++ src/main/java/at/fh/ima/swengs/swengular/repository/MovieRepository.java
@@ -1,5 +1,9 @@
 package at.fh.ima.swengs.swengular.repository;
 
+//DEPRECATED
+//Will use TMDBmovie instead
+/*
+
 import at.fh.ima.swengs.swengular.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -17,3 +21,4 @@ public interface MovieRepository extends PagingAndSortingRepository<Movie, Long>
     Set<Movie> removeByTitle(@Param("title") String title);
 
 }
+*/
