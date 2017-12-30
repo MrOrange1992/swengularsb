1090b122f28c26d2bfc1451adeb4d6c56fd9f9e9
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-12-30 15:11:12 +0100

    rep deprecated, will use Genre class from Tmdb API

--- src/main/java/at/fh/ima/swengs/swengular/repository/GenreRepository.java
+++ src/main/java/at/fh/ima/swengs/swengular/repository/GenreRepository.java
@@ -1,11 +1,19 @@
 package at.fh.ima.swengs.swengular.repository;
 
-import at.fh.ima.swengs.swengular.model.Genre;
+
+
+//DEPRECATED
+//using TMDB Genre instead
+/*
+
+//import at.fh.ima.swengs.swengular.model.Genre;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Set;
+
+
 
 @RepositoryRestResource
 public interface GenreRepository extends PagingAndSortingRepository<Genre, Long>
@@ -17,3 +25,4 @@ public interface GenreRepository extends PagingAndSortingRepository<Genre, Long>
     Genre save(Genre genre);
 
 }
+*/
